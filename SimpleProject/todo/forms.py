f7c1c9bb3ec75618b997111b3dc085e313778f8c
hitesh-hkx <44205283+hitesh-hkx@users.noreply.github.com>
2018-10-21 18:32:43 +0530

    Small Changes in Attributes

--- SimpleProject/todo/forms.py
+++ SimpleProject/todo/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class TodoForm(forms.Form):
     text = forms.CharField(max_length=100,widget=forms.TextInput(
     attrs={
-        'placeholder':'Enter todo e.g. Delete junk files',
+        'placeholder':'Add a new Todo',
         'class':'form-control',
         'aria-label':'Todo',
         'aria-describedby':'add-btn'
